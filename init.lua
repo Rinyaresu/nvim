7ead44b64fbcdb6de5fd98fbfc50d21d2ff9edc7
Kaique Linhares <79602114+Rinyaresu@users.noreply.github.com>
2023-03-10 10:20:14 -0300

    feat: sets

--- init.lua
+++ init.lua
@@ -8,13 +8,20 @@ vim.opt.mouse = 'a'
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.hlsearch = false
-vim.opt.wrap = true
+vim.opt.incsearch = true
+vim.opt.wrap = false
 vim.opt.breakindent = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = false
 vim.opt.signcolumn = 'yes'
-vim.opt.termguicolors = true
+vim.opt.smartindent = true
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv('HOME') .. '/.vim/undodir'
+vim.opt.undofile = true
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = '80'
 
 -- Space as leader key
 vim.g.mapleader = ' '

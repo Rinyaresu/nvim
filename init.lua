a184a646db1509a76f8c23777ec30a10598490c9
Kaique Linhares <79602114+Rinyaresu@users.noreply.github.com>
2023-03-10 20:39:05 -0300

    refactor: remap

--- init.lua
+++ init.lua
@@ -3,37 +3,7 @@
 -- ========================================================================== --
 
 require('core.set')
-
--- Shortcuts
-vim.keymap.set({ 'n', 'x', 'o' }, '<leader>h', '^')
-vim.keymap.set({ 'n', 'x', 'o' }, '<leader>l', 'g_')
-vim.keymap.set('n', '<leader>a', ':keepjumps normal! ggVG<cr>')
-
--- Basic clipboard interaction
-vim.keymap.set({ 'n', 'x' }, 'y', '"+y')
-vim.keymap.set({ 'n', 'x' }, 'p', '"+p')
-
--- Delete text
-vim.keymap.set({ 'n', 'x' }, 'x', '"_x')
-
--- Commands
-vim.keymap.set('n', '<leader>w', '<cmd>write<cr>')
-vim.keymap.set('n', '<leader>bq', '<cmd>bdelete<cr>')
-vim.keymap.set('n', '<leader>bl', '<cmd>buffer #<cr>')
-
-vim.keymap.set('n', 'H', '<cmd>BufferLineCyclePrev<cr><CR>')
-vim.keymap.set('n', 'L', '<cmd>BufferLineCycleNext<CR>')
-
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
-
-vim.keymap.set('n', 'J', 'mzJ`z')
-
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
-vim.keymap.set('n', 'n', 'nzzzv')
-vim.keymap.set('n', 'N', 'Nzzzv')
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+require('core.remap')
 
 -- ========================================================================== --
 -- ==                               COMMANDS                               == --
@@ -236,20 +206,13 @@ lazy.setup({
 vim.cmd.colorscheme('tokyonight-night')
 
 ---
--- vim-bbye
----
-vim.keymap.set('n', '<leader>bc', '<cmd>Bdelete<CR>')
-
----
 -- autopairs
 ---
-
 require("nvim-autopairs").setup {}
 
 ---
 -- indent blankline
 ---
-
 require("indent_blankline").setup {
 	char = '▏',
 	show_trailing_blankline_indent = false,
@@ -263,13 +226,9 @@ require("indent_blankline").setup {
 ---
 require 'colorizer'.setup()
 
-
--- See :help lualine.txt
---
-
--- Eviline config for lualine
--- Author: shadmansaleh
--- Credit: glepnir
+---
+-- lualine
+---
 local lualine = require('lualine')
 
 -- Color table for highlights
@@ -593,14 +552,6 @@ require('gitsigns').setup({
 -- Telescope
 ---
 -- See :help telescope.builtin
-vim.keymap.set('n', '<leader>?', '<cmd>Telescop/e oldfiles<cr>')
-vim.keymap.set('n', '<leader><space>', '<cmd>Telescope buffers<cr>')
-vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
-vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
-vim.keymap.set('n', '<leader>fd', '<cmd>Telescope diagnostics<cr>')
-vim.keymap.set('n', '<leader>fs', '<cmd>Telescope current_buffer_fuzzy_find<cr>')
-vim.keymap.set('n', '<leader>fr', '<cmd>Telescope oldfiles<CR>')
-vim.keymap.set('n', '<leader><tab>', '<cmd>Telescope git_status<CR>')
 
 require('telescope').load_extension('fzf')
 
@@ -609,16 +560,8 @@ require('telescope').load_extension('fzf')
 ---
 -- See :help mind.setup
 
-vim.keymap.set('n', '<leader>mp', ':MindOpenMain<CR>')
-vim.keymap.set('n', '<leader>mc', ':MindClose<CR>')
-
 require 'mind'.setup {}
 
-
----
--- Undotree
---
-vim.keymap.set('n', '<leader>fu', ":UndotreeToggle<CR> <BAR> :UndotreeFocus<CR>")
 
 ---
 -- toggleterm
---
+++ lua/core/remap.lua
@@ -0,0 +1,48 @@
+vim.g.mapleader = ' '
+
+-- Shortcuts
+vim.keymap.set({ 'n', 'x', 'o' }, '<leader>h', '^')
+vim.keymap.set({ 'n', 'x', 'o' }, '<leader>l', 'g_')
+vim.keymap.set('n', '<leader>a', ':keepjumps normal! ggVG<cr>')
+
+-- Basic clipboard interaction
+vim.keymap.set({ 'n', 'x' }, 'y', '"+y')
+vim.keymap.set({ 'n', 'x' }, 'p', '"+p')
+
+-- Delete text
+vim.keymap.set({ 'n', 'x' }, 'x', '"_x')
+
+-- Commands
+vim.keymap.set('n', '<leader>w', '<cmd>write<cr>')
+vim.keymap.set('n', '<leader>bq', '<cmd>bdelete<cr>')
+vim.keymap.set('n', '<leader>bl', '<cmd>buffer #<cr>')
+
+vim.keymap.set('n', 'H', '<cmd>BufferLineCyclePrev<cr><CR>')
+vim.keymap.set('n', 'L', '<cmd>BufferLineCycleNext<CR>')
+
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+vim.keymap.set('n', 'J', 'mzJ`z')
+
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+
+vim.keymap.set('n', '<leader>fu', ":UndotreeToggle<CR> <BAR> :UndotreeFocus<CR>")
+
+vim.keymap.set('n', '<leader>mp', ':MindOpenMain<CR>')
+vim.keymap.set('n', '<leader>mc', ':MindClose<CR>')
+
+vim.keymap.set('n', '<leader>bc', '<cmd>Bdelete<CR>')
+
+vim.keymap.set('n', '<leader><space>', '<cmd>Telescope buffers<cr>')
+vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
+vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
+vim.keymap.set('n', '<leader>fd', '<cmd>Telescope diagnostics<cr>')
+vim.keymap.set('n', '<leader>fs', '<cmd>Telescope current_buffer_fuzzy_find<cr>')
+vim.keymap.set('n', '<leader>fr', '<cmd>Telescope oldfiles<CR>')
+vim.keymap.set('n', '<leader><tab>', '<cmd>Telescope git_status<CR>')

5b197e0cb1efb43e3bc2907710b551a724295322
Kaique Linhares <79602114+Rinyaresu@users.noreply.github.com>
2023-03-10 21:02:22 -0300

    refactor: big plugins

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,39 @@
+---
+-- Treesitter
+---
+-- See :help nvim-treesitter-modules
+require('nvim-treesitter.configs').setup({
+	highlight = {
+		enable = true,
+	},
+	rainbow = {
+		enable = true,
+		-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+		extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+		max_file_lines = nil, -- Do not enable for files with more than n lines, int
+		-- colors = {}, -- table of hex strings
+		-- termcolors = {} -- table of colour name strings
+	},
+	-- :help nvim-treesitter-textobjects-modules
+	textobjects = {
+		select = {
+			enable = true,
+			lookahead = true,
+			keymaps = {
+				['af'] = '@function.outer',
+				['if'] = '@function.inner',
+				['ac'] = '@class.outer',
+				['ic'] = '@class.inner',
+			}
+		},
+	},
+	ensure_installed = {
+		'javascript',
+		'typescript',
+		'tsx',
+		'lua',
+		'css',
+		'json',
+		'ruby',
+	},
+})

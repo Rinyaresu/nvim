1c5f33182bfc69899ac8d446f527ae5918b2b2fe
Kaique Linhares <79602114+Rinyaresu@users.noreply.github.com>
2023-03-12 14:40:23 -0300

    feat: unsure more lang in TS

--- lua/plugin/treesitter.lua
+++ lua/plugin/treesitter.lua
@@ -4,51 +4,62 @@
 -- See :help nvim-treesitter-modules
 --
 require('treesitter-context').setup {
-	enable = true,           -- Enable this plugin (Can be enabled/disabled later via commands)
-	max_lines = 0,           -- How many lines the window should span. Values <= 0 mean no limit.
-	min_window_height = 0,   -- Minimum editor window height to enable context. Values <= 0 mean no limit.
-	line_numbers = true,
-	multiline_threshold = 20, -- Maximum number of lines to collapse for a single context line
-	trim_scope = 'outer',    -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-	mode = 'cursor',         -- Line used to calculate context. Choices: 'cursor', 'topline'
-	-- Separator between context and content. Should be a single character string, like '-'.
-	-- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-	separator = nil,
-	zindex = 20, -- The Z-index of the context window
+  enable = true,            -- Enable this plugin (Can be enabled/disabled later via commands)
+  max_lines = 0,            -- How many lines the window should span. Values <= 0 mean no limit.
+  min_window_height = 0,    -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+  line_numbers = true,
+  multiline_threshold = 20, -- Maximum number of lines to collapse for a single context line
+  trim_scope = 'outer',     -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+  mode = 'cursor',          -- Line used to calculate context. Choices: 'cursor', 'topline'
+  -- Separator between context and content. Should be a single character string, like '-'.
+  -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+  separator = nil,
+  zindex = 20, -- The Z-index of the context window
 }
 
 require('nvim-treesitter.configs').setup({
-	highlight = {
-		enable = true,
-	},
-	rainbow = {
-		enable = true,
-		-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-		extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-		max_file_lines = nil, -- Do not enable for files with more than n lines, int
-		-- colors = {}, -- table of hex strings
-		-- termcolors = {} -- table of colour name strings
-	},
-	-- :help nvim-treesitter-textobjects-modules
-	textobjects = {
-		select = {
-			enable = true,
-			lookahead = true,
-			keymaps = {
-				['af'] = '@function.outer',
-				['if'] = '@function.inner',
-				['ac'] = '@class.outer',
-				['ic'] = '@class.inner',
-			}
-		},
-	},
-	ensure_installed = {
-		'javascript',
-		'typescript',
-		'tsx',
-		'lua',
-		'css',
-		'json',
-		'ruby',
-	},
+  highlight = {
+    enable = true,
+  },
+  rainbow = {
+    enable = true,
+    -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+    max_file_lines = nil, -- Do not enable for files with more than n lines, int
+    -- colors = {}, -- table of hex strings
+    -- termcolors = {} -- table of colour name strings
+  },
+  -- :help nvim-treesitter-textobjects-modules
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true,
+      keymaps = {
+        ['af'] = '@function.outer',
+        ['if'] = '@function.inner',
+        ['ac'] = '@class.outer',
+        ['ic'] = '@class.inner',
+      }
+    },
+  },
+  ensure_installed = {
+    'javascript',
+    'typescript',
+    'tsx',
+    'lua',
+    'css',
+    'json',
+    'ruby',
+    'html',
+    'bash',
+    'python',
+    'fish',
+    'toml',
+    'yaml',
+    'vim',
+    'latex',
+    'elixir',
+    'sql',
+    'scss',
+  },
 })

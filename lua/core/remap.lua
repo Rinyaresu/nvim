4ec85d1264ad3e15e04107cb12beb114ee33971a
Kaique Linhares <79602114+Rinyaresu@users.noreply.github.com>
2023-03-12 11:34:40 -0300

    refactor: remap

--- lua/core/remap.lua
+++ lua/core/remap.lua
@@ -16,33 +16,36 @@ vim.keymap.set({ 'n', 'x' }, 'x', '"_x')
 -- Delete line
 vim.keymap.set({ 'n', 'x' }, 'd', '"+d')
 
--- Commands
+-- Buffer
 vim.keymap.set('n', '<leader>w', '<cmd>write<cr>')
+vim.keymap.set('n', '<leader>bc', '<cmd>Bdelete<CR>')
 vim.keymap.set('n', '<leader>bq', '<cmd>bdelete<cr>')
 vim.keymap.set('n', '<leader>bl', '<cmd>buffer #<cr>')
-
 vim.keymap.set('n', 'H', '<cmd>BufferLineCyclePrev<cr><CR>')
 vim.keymap.set('n', 'L', '<cmd>BufferLineCycleNext<CR>')
 
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+-- Move lines up and down
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 
+-- Motions
 vim.keymap.set('n', 'J', 'mzJ`z')
-
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
+
+-- Replace word
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
-
+-- Undo tree
 vim.keymap.set('n', '<leader>fu', ":UndotreeToggle<CR> <BAR> :UndotreeFocus<CR>")
 
+-- Mind
 vim.keymap.set('n', '<leader>mp', ':MindOpenMain<CR>')
 vim.keymap.set('n', '<leader>mc', ':MindClose<CR>')
 
-vim.keymap.set('n', '<leader>bc', '<cmd>Bdelete<CR>')
-
+-- Telescope
 vim.keymap.set('n', '<leader><space>', '<cmd>Telescope buffers<cr>')
 vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
 vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
@@ -52,9 +55,11 @@ vim.keymap.set('n', '<leader>fr', '<cmd>Telescope oldfiles<CR>')
 vim.keymap.set('n', '<leader><tab>', '<cmd>Telescope git_status<CR>')
 vim.keymap.set('n', '<leader>fb', '<cmd>Telescope lsp_document_symbols<CR>')
 
+-- Persistence sessions
 vim.api.nvim_set_keymap("n", "<leader>qs", [[<cmd>lua require("persistence").load()<cr>]], {})
 vim.api.nvim_set_keymap("n", "<leader>ql", [[<cmd>lua require("persistence").load({ last = true })<cr>]], {})
 
+-- Git
 vim.keymap.set('n', '<leader>gg', '<cmd>:LazyGit<CR>')
 vim.keymap.set('n', '<leader>gd', '<cmd>:DiffviewOpen<CR>')
 vim.keymap.set('n', '<leader>gl', '<cmd>:DiffviewFileHistory<CR>')
